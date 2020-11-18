f15facd9433bd15ddfe4a5b70353cac470c3ea91
kankuro12 <cms111000111@gmail.com>
2020-11-18 18:58:21 +0545

    user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -101,7 +101,7 @@ class AuthController extends Controller
             'expires_at' => Carbon::parse(
                 $tokenResult->token->expires_at
             )->toDateTimeString(),
-            'newuser'=>$new
+            'newuser'=>$user->name==""
         ]);
     }
   
